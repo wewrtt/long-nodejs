8ee98ae36a5a80e52fbbe3781907efd539b88dfb
trinh thanh long <trinhthanhlong2842000@gmail.com>
2022-02-05 16:44:28 +0700

    commit

--- src/app/cotrollers/StieControllers.js
+++ src/app/cotrollers/StieControllers.js
@@ -7,11 +7,19 @@ class StieControllers {
     }
     index(req, res, next) {
         //viet kieu promis ham find trong mongoose tra ve 1 mang json
+        /*
+        var courses = await model.find({});
+        */ 
         model
             .find({})
             .then((courses) => {
                 courses = changeObject.multiMongooseObject(courses);
-                res.render('home', { courses });
+                // phân trang cơ bản
+                var n =parseInt(req.query.page) || 1;//n
+                var x= 3;//x
+                var start= (n-1)*x;
+                var end=n*x;
+                res.render('home', { courses:courses.slice(start, end)});
             })
             .catch(next);
         //viet kieu if else
